a1614d34c1ff895f1664bb93175e1c85643e42dd
John Moseley <johnmoseley@Wecancodeit.org>
2023-03-16 14:06:30 -0400

    With Controlles

---
+++ GameService/Controllers/BoardGameApiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataModel;
+using GameService.Data;
+
+namespace GameService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardGameApiController : ControllerBase
+    {
+        private readonly GameContext _context;
+
+        public BoardGameApiController(GameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BoardGameApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BoardGameModel>>> GetBoardGames()
+        {
+          if (_context.BoardGames == null)
+          {
+              return NotFound();
+          }
+            return await _context.BoardGames.ToListAsync();
+        }
+
+        // GET: api/BoardGameApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BoardGameModel>> GetBoardGameModel(int id)
+        {
+          if (_context.BoardGames == null)
+          {
+              return NotFound();
+          }
+            var boardGameModel = await _context.BoardGames.FindAsync(id);
+
+            if (boardGameModel == null)
+            {
+                return NotFound();
+            }
+
+            return boardGameModel;
+        }
+
+        // PUT: api/BoardGameApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBoardGameModel(int id, BoardGameModel boardGameModel)
+        {
+            if (id != boardGameModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(boardGameModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BoardGameModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BoardGameApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BoardGameModel>> PostBoardGameModel(BoardGameModel boardGameModel)
+        {
+          if (_context.BoardGames == null)
+          {
+              return Problem("Entity set 'GameContext.BoardGames'  is null.");
+          }
+            _context.BoardGames.Add(boardGameModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBoardGameModel", new { id = boardGameModel.Id }, boardGameModel);
+        }
+
+        // DELETE: api/BoardGameApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBoardGameModel(int id)
+        {
+            if (_context.BoardGames == null)
+            {
+                return NotFound();
+            }
+            var boardGameModel = await _context.BoardGames.FindAsync(id);
+            if (boardGameModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.BoardGames.Remove(boardGameModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BoardGameModelExists(int id)
+        {
+            return (_context.BoardGames?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ GameService/Controllers/PublisherApiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataModel;
+using GameService.Data;
+
+namespace GameService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherApiController : ControllerBase
+    {
+        private readonly GameContext _context;
+
+        public PublisherApiController(GameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PublisherApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PublisherModel>>> GetPublishers()
+        {
+          if (_context.Publishers == null)
+          {
+              return NotFound();
+          }
+            return await _context.Publishers.ToListAsync();
+        }
+
+        // GET: api/PublisherApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PublisherModel>> GetPublisherModel(int id)
+        {
+          if (_context.Publishers == null)
+          {
+              return NotFound();
+          }
+            var publisherModel = await _context.Publishers.FindAsync(id);
+
+            if (publisherModel == null)
+            {
+                return NotFound();
+            }
+
+            return publisherModel;
+        }
+
+        // PUT: api/PublisherApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublisherModel(int id, PublisherModel publisherModel)
+        {
+            if (id != publisherModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(publisherModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PublisherApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PublisherModel>> PostPublisherModel(PublisherModel publisherModel)
+        {
+          if (_context.Publishers == null)
+          {
+              return Problem("Entity set 'GameContext.Publishers'  is null.");
+          }
+            _context.Publishers.Add(publisherModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPublisherModel", new { id = publisherModel.Id }, publisherModel);
+        }
+
+        // DELETE: api/PublisherApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublisherModel(int id)
+        {
+            if (_context.Publishers == null)
+            {
+                return NotFound();
+            }
+            var publisherModel = await _context.Publishers.FindAsync(id);
+            if (publisherModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Publishers.Remove(publisherModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PublisherModelExists(int id)
+        {
+            return (_context.Publishers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
